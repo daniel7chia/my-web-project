cf24c3f9bdf571c5aa2406965dbe942846d1769d
daniel7chia <daniel_chia_@hotmail.com>
2022-02-02 17:56:14 +0800

    4th commit

--- src/main/java/first/webapp/DatabaseOperations.java
+++ src/main/java/first/webapp/DatabaseOperations.java
@@ -19,7 +19,7 @@ public class DatabaseOperations {
         " (?, ?, ?);";
 
     private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
-    private static final String SELECT_ALL_USERS = "select * from userdetails";
+    private static final String SELECT_ALL_USERS = "select * from user";
     private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
     private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
 
